c090961b51050c8f7f7da878ec563defc0f6171f
Caleb <cschumacher7777777@gmail.com>
2023-06-24 15:25:55 -0500

    Linked index view with detail view

--- ecom/urls.py
+++ ecom/urls.py
@@ -21,5 +21,6 @@ from ecomsite import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
+    path('<int:id>/', views.detail, name='detail'),
 
 ]
--- ecomsite/views.py
+++ ecomsite/views.py
@@ -8,13 +8,18 @@ from django.core.paginator import Paginator
 def index(request):
     product_objects = Products.objects.all()
 
+    # Search Function
     item_name = request.GET.get('item_name')
     if item_name != '' and item_name is not None:
         product_objects = product_objects.filter(title__icontains=item_name)
 
+    # Paginator
     paginator = Paginator(product_objects, 3)
     page = request.GET.get('page')
     product_objects = paginator.get_page(page)
 
     return render(request, 'shop/index.html', {'product_objects':product_objects})
 
+def detail(request, id):
+    product_object = Products.objects.get(id=id)
+    return render(request, 'shop/detail.html', {'product_object':product_object})
